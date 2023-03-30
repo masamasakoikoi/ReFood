910fc31192e85aae34a0fd6489ef99a34fd01d2a
masamasakoikoi <koichan1996@icloud.com>
2023-03-30 00:03:47 +0000

    update_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,33 @@
 Rails.application.routes.draw do
-  # 顧客用
-  # URL/users/sign_in...
-  devise_for:users,skip:[:passwords],controllers:{
-    registrations:"public/registrations",
-    sessions:'public/sessions'
-  }
-  
   # 管理者用
   # URL/admin/sign_in...
   devise_for:admin,skip:[:registrations,:passwords],controllers:{
     sessions:"admin/sessions"
   }
   
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/unsubscribe'
-    get 'users/status'
+  #会員用
+  # URL/users/sign_in...
+  devise_for:users,skip:[:passwords],controllers:{
+    registrations:"public/registrations",
+    sessions:'public/sessions'
+  }
+  
+  namespace :admin do
+    
   end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
+  
+  scope module: :public do
+    resources :users ,only:[:show, :edit, :update, :unsubscribe, :status] do
+    end
+    
+    resources :items ,only:[:index, :show] do
+    end
+
     get 'homes/top'
     get 'homes/about'
   end
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
